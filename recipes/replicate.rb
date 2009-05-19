d8e90e40e860ef904df2004c4077546888423675
Graeme Mathieson <mathie@woss.name>
2009-05-19 17:24:22 +0100

    Replicate the production database environment to our local environment.

---
+++ recipes/replicate.rb
@@ -0,0 +1,19 @@
+desc <<-TEXT
+Clone the deployment environment to your current environment. This will back
+up the current database and any assets from the production environment, copy
+them locally and load them. WARNING: This will totally and utterly destroy the
+current data contents of your local environment. Don't do this unless you're
+paying attention!
+TEXT
+task :replicate, :roles => [ :db ], :only => { :primary => true } do
+  source_env = fetch(:rails_env, 'production')
+  target_env = ENV['TARGET_ENV'] || 'development'
+
+  find_and_execute_task("db:download")
+  find_and_execute_task("assets:download")
+
+  asset_directories.each do |dir|
+    run_locally "rm -rf #{dir}"
+  end
+  run_locally "rake RAILS_ENV=#{target_env} SOURCE_ENV=#{source_env} db:backup:load assets:backup:load"
+end
