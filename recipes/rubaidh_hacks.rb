1da20bd4eedcef1bd39e53e649350dc82eb7d8e8
Graeme Mathieson <mathie@woss.name>
2009-05-19 16:32:35 +0100

    Bit of an explanation of rubaidh_hacks.rb.

--- recipes/rubaidh_hacks.rb
+++ recipes/rubaidh_hacks.rb
@@ -1,3 +1,6 @@
+# If you're going to fork this project, this is probably the first file you
+# want to nuke. It's a collection of hacks that are specific to our deployment
+# environment(s) and probably won't be much use to anybody else.
 on :load do
   case fetch(:host, nil)
   when "balblair.rubaidh.com" # Welcome to OpenSolaris.  *sigh*
