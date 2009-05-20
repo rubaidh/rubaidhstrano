ca03c43cc5f9dd63d269ad78192c135e262f5e79
Graeme Mathieson <mathie@woss.name>
2009-05-20 05:15:46 +0100

    Host isn't available on load in a multistage environment.

--- recipes/rubaidh_hacks.rb
+++ recipes/rubaidh_hacks.rb
@@ -2,8 +2,5 @@
 # want to nuke. It's a collection of hacks that are specific to our deployment
 # environment(s) and probably won't be much use to anybody else.
 on :load do
-  case fetch(:host, nil)
-  when "balblair.rubaidh.com" # Welcome to OpenSolaris.  *sigh*
-    set :tar, "gtar"
-  end
+  set(:tar) { fetch(:host, nil) == "balblair.rubaidh.com" ? "gtar" : "tar" }
 end
