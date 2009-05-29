c479d71a4cfc178839f97f585c0b055e107247b8
Graeme Mathieson <mathie@woss.name>
2009-05-29 12:57:33 +0100

    I think that's about what I need to remove a repository cache.

---
+++ recipes/cache.rb
@@ -0,0 +1,17 @@
+namespace :deploy do
+  namespace :cache do
+    namespace :code do
+      desc <<-TEXT
+        Remove the cached copy of the code. This can sometimes be helpful
+        when, for example, you've changed the upstream location of a git
+        submodule. Git doesn't seem to want to update its working copy with
+        that information, so chances are the easiest thing to do is nuke the
+        cached copy and start again.
+      TEXT
+      task :remove :except => { :no_release => true } do
+        cache = strategy.send(:repository_cache)
+        run "[ -d #{cache} ] && rm -rf #{cache}"
+      end
+    end
+  end
+end
